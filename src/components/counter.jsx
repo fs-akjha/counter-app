53951f849653a5607c159930bae4cb255f023900
Akash Jha <79706803+fs-akjha@users.noreply.github.com>
2022-05-10 19:40:21 +0530

    With Counters features

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state={
-        count:0
+        value: this.props.counter.value
     };
 
     // constructor() {
@@ -10,27 +10,32 @@ class Counter extends Component {
     //     this.handleIncrement= this.handleIncrement.bind(this);
     // }
 
-    handleIncrement =() => {
-        console.log('Increment Clicked', this);
+    handleIncrement =(product) => {
+        this.setState({ value: this.state.value+1 })
     }
 
-    render() { 
+    doHandleIncrement = () => {
+        this.handleIncrement({ id: 1});
+    }
+
+    render() {
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+                <button onClick={this.doHandleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
         );
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 bg-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const { count }=this.state;
+        const { value: count }=this.state;
         return count === 0 ? 'Zero' : count;
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Counter from './components/counter';
+import Counters from './components/counters';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Counter />
+  <Counters />
 );
 
 // If you want to start measuring performance in your app, pass a function
